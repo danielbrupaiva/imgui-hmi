17a655fb4ee890e07e97bcf5c8728efda78c8a8f
Daniel Paiva <danielbrug@gmail.com>
2024-08-26 19:04:26 -0300

    Add IMGUI.cmake

---
+++ _cmake/IMGUI.cmake
@@ -0,0 +1,12 @@
+include(FetchContent)
+#---------------------
+FetchContent_Declare(
+        imgui
+#        SOURCE_DIR /home/daniel/CLionProjects/BeaglePlay/vendor/imgui
+        GIT_REPOSITORY https://github.com/danielbrupaiva/imgui.git
+        GIT_TAG cmake
+)
+FetchContent_MakeAvailable(imgui)
+FetchContent_GetProperties(imgui)
+#---------------------
+target_link_libraries(${PROJECT_NAME} PUBLIC imgui)
