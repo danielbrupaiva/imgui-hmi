5cd0ad34dac30a66f2825bca3963d501d8d52630
Daniel Paiva <danielbrug@gmail.com>
2024-08-26 19:33:41 -0300

    Review App::Spec

--- App/globals.hpp
+++ App/globals.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 #include "logger.hpp"
+#include "spec.hpp"
 
+using namespace App;
 using namespace Core;
+
 namespace Global {
 static Logger logger{"App", spdlog::level::debug};
+
+static const Spec hmi_spec = {
+    .backend_type       = eBackend::OPENGLES,
+    .shader_version     = GLSL_VERSION["ES 3.0"],
+    .title              = "IMGUI HMI",
+    .window_size        = ImVec2(),
+    .bg_color           = ImVec4(),
+    .vsync              = 1
+};
+
 };//namespace Global
--- App/spec.hpp
+++ App/spec.hpp
@@ -5,15 +5,35 @@
 #pragma once
 
 #include <iostream>
+#include <map>
 #include "imgui.h"
 
-namespace App
-{
-struct Spec
-{
+namespace App {
+
+enum class eBackend { OPENGL, OPENGLES, VULKAN };
+
+// GLSL version
+static std::map<std::string, std::string> GLSL_VERSION = {
+    {"2.0", "#version 110"},
+    {"2.1", "#version 120"},
+    {"3.0", "#version 130"},
+    {"3.1", "#version 140"},
+    {"3.2", "#version 150"},
+    {"3.3", "#version 330 core"},
+    {"4.0", "#version 400 core"},
+    {"4.1", "#version 410 core"},
+    {"4.2", "#version 410 core"},
+    {"4.3", "#version 430 core"},
+    {"ES 2.0", "#version 100"},
+    {"ES 3.0", "#version 300 es"}
+};
+
+struct Spec {
+    eBackend backend_type;
+    std::string shader_version = GLSL_VERSION["2.0"];
     std::string title;
     ImVec2 window_size;
     ImVec4 bg_color;
-    int8_t enable_vsync;
+    int8_t vsync;
 };
 } // App
