c5cd82620e5ac7eaa824e8d57e0b1a3605702f26
Daniel Paiva <danielbrug@gmail.com>
2024-09-14 08:31:35 -0300

    Add debug buttons

--- App/main.cpp
+++ App/main.cpp
@@ -11,10 +11,29 @@ void debug_screen(IMGUI *app)
 	if (show_demo_window)
 		ImGui::ShowDemoWindow(&show_demo_window);
 
-	ImGui::Begin("Debug");
+	ImGui::Begin("Debug", NULL, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav);
 	{
 		if (ImGui::Button("IMGUI")) {
 			show_demo_window = !show_demo_window;
+		}
+		if (ImGui::Button("SCREEN1")) {
+			screen_manager.set_current_state(ScreenManager::eState::SCREEN1);
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("SCREEN2")) {
+			screen_manager.set_current_state(ScreenManager::eState::SCREEN2);
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("SCREEN3")) {
+			screen_manager.set_current_state(ScreenManager::eState::SCREEN3);
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("SCREEN4")) {
+			screen_manager.set_current_state(ScreenManager::eState::SCREEN4);
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("SCREEN5")) {
+			screen_manager.set_current_state(ScreenManager::eState::SCREEN5);
 		}
 		if (ImGui::Button("EXIT")) {
 			app->close();
