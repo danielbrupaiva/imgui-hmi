17c98ae7958255de3200c60c0427ce331f282a62
Daniel Paiva <danielbrug@gmail.com>
2024-09-18 23:28:31 -0300

    Add Math utils

--- Core/utils.hpp
+++ Core/utils.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-// Macro for maximum of two values
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
-// Macro for minimum of two values
-#define MIN(x, y) ((x) < (y) ? (x) : (y))
-// Macro for average of two values
-#define AVG(x, y) (((x) + (y)) / 2)
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
 
 #define TOGGLE(var) (var = ! var)
 #define NOT(expr) (!(expr))
@@ -14,8 +14,42 @@
 
 namespace Core
 {
-class Utils
+namespace Utils
 {
+namespace Math
+{
+// Base case: single argument
+template<typename Type>
+constexpr Type MAX(Type value)
+{ return value; }
+// Recursive case: multiple arguments
+template<typename Type, typename... Args>
+constexpr Type MAX(Type first, Args... args)
+{ return (first > MAX(args...)) ? first : MAX(args...); }
+// Vector container
+template<typename Type, typename... Args>
+constexpr Type MAX(const std::vector<Type> &vec)
+{ return *std::max_element(vec.begin(), vec.end()); }
 
+template<typename Type>
+static constexpr Type MIN(Type value)
+{ return value; };
+
+template<typename Type, typename... Args>
+static constexpr Type MIN(Type first, Args... args)
+{ return (first < MIN(args...)) ? first : MIN(args...); };
+
+template<typename Type, typename... Args>
+constexpr Type MIN(const std::vector<Type> &vec)
+{ return *std::min_element(vec.begin(), vec.end()); }
+
+template<typename Type>
+static constexpr double AVG(Type value)
+{ return static_cast<double>(value); };
+
+template<typename Type, typename... Args>
+static constexpr double AVG(Type first, Args... args)
+{ return (static_cast<double>(first) + average_value(args...)) / (sizeof...(args) + 1); };
 };
-} // Core
+};
+};
