b23fb1296977e1c38d40d86224693a6bcfe75c5e
Daniel Paiva <danielbrug@gmail.com>
2024-09-27 16:48:04 -0300

    Remove pointer to image

--- App/screens/screen_1.hpp
+++ App/screens/screen_1.hpp
@@ -8,17 +8,16 @@ namespace App
 {
 class Screen1: public IScreen
 {
-	std::unique_ptr<Image> splash;
+	Image splash{"./resources/morpheus.jpg"};
 public:
 	Screen1(const std::string_view &title, const ImVec2 &size)
 		: IScreen(title, size)
 	{
 		logger.debug("Constructor {}", get_title());
-		splash = std::make_unique<Image>("./resources/morpheus.jpg");
 	}
 	void render() override
 	{
-		splash->operator()(ImGui::GetContentRegionAvail(), ImGui::GetCursorPos());
+		splash(ImGui::GetContentRegionAvail(), ImGui::GetCursorPos());
 	}
 };
 }
