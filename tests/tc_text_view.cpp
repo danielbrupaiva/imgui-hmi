3aa45886eca6c77fc712f7d089f7826eb875dc2d
Daniel Paiva <danielbrug@gmail.com>
2024-09-29 12:18:36 -0300

    Add set layout window positon

--- tests/tc_text_view.cpp
+++ tests/tc_text_view.cpp
@@ -10,7 +10,7 @@ TEST_CASE("TextView class")
 	{
 		auto label1 = "TEXT#1";
 		ImVec2 position{25.0f, 50.0f};
-		auto font_size = static_cast<int8_t>(App::IMGUI::FontSize::_22px);
+		auto font_size = App::Font::Size::_36px;
 
 		App::TextView text_view1(label1);
 		REQUIRE(text_view1.get_label() == label1);
