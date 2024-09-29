fb4d8def9e3c356361a7a81955ce107d25c78673
Daniel Paiva <danielbrug@gmail.com>
2024-09-29 12:18:29 -0300

    Add set layout window positon

--- App/screens/screen_2.hpp
+++ App/screens/screen_2.hpp
@@ -8,7 +8,13 @@ namespace App
 {
 class Screen2: public IScreen
 {
-	TextView text_view{get_title(), Font::Size::_36px};
+	TextView text_view1{"NONE", Font::Size::_36px};
+	TextView text_view2{"CENTER", Font::Size::_36px};
+	TextView text_view3{"TOP", Font::Size::_36px};
+	TextView text_view4{"BOTTON", Font::Size::_36px};
+	TextView text_view5{"LEFT", Font::Size::_36px};
+	TextView text_view6{"RIGHT", Font::Size::_36px};
+
 
 public:
 	Screen2(const std::string_view &title, const ImVec2 &size)
@@ -18,7 +24,17 @@ public:
 	}
 	void render() override
 	{
-		text_view(IWidget::Layout::CENTER);
+		text_view1(IWidget::Layout::NONE);
+		text_view2(IWidget::Layout::WINDOW_CENTER);
+		text_view3(IWidget::Layout::WINDOW_TOP);
+		text_view4(IWidget::Layout::WINDOW_BOTTON);
+		text_view5(IWidget::Layout::WINDOW_LEFT);
+		text_view6(IWidget::Layout::WINDOW_RIGHT);
+//		text_view2(IWidget::Layout::CENTER);
+//		text_view3(IWidget::Layout::TOP);
+//		text_view4(IWidget::Layout::BOTTON);
+//		text_view5(IWidget::Layout::LEFT);
+//		text_view6(IWidget::Layout::RIGHT);
 	}
 };
 }
