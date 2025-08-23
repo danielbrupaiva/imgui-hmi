69f6f9e2117caaa51ec6683bd7c8eebec9023bdb
Daniel Paiva <danielbrug@gmail.com>
2025-08-23 12:22:58 -0300

    update IMGUI.cmake

--- cmake/IMGUI.cmake
+++ cmake/IMGUI.cmake
@@ -11,9 +11,9 @@ include(FetchContent)
 #---------------------
 FetchContent_Declare(
         imgui
-        SOURCE_DIR $ENV{VENDOR_FOLDER}/imgui
-#        GIT_REPOSITORY https://github.com/danielbrupaiva/imgui.git
-#        GIT_TAG cmake
+#        SOURCE_DIR $ENV{VENDOR_FOLDER}/imgui
+        GIT_REPOSITORY https://github.com/danielbrupaiva/imgui.git
+        GIT_TAG cmake
 )
 FetchContent_MakeAvailable(imgui)
 FetchContent_GetProperties(imgui)
