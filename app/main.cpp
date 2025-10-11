0d4cdfdcbdaad11077da133561a2c4a4c32f2868
Daniel Paiva <danielbrug@gmail.com>
2025-10-11 09:13:51 -0300

    Change to OpenGLES version 3.0

--- app/main.cpp
+++ app/main.cpp
@@ -8,8 +8,10 @@ int main() {
     spec.window_spec.fullscreen = false;
     spec.window_spec.width = 1280;
     spec.window_spec.height = 720;
-    spec.gl_shader_version = Core::Application::GLSL_VERSION["4.60"];
-    spec.glfw_window_hints.clientApi = Core::Application::ClientApi::OpenGl;
+    spec.gl_shader_version = Core::Application::GLSL_VERSION["ES3.00"];
+    spec.glfw_window_hints.contextVersionMajor = 3;
+    spec.glfw_window_hints.contextVersionMinor = 0;
+    spec.glfw_window_hints.clientApi = Core::Application::ClientApi::OpenGles;
     spec.glfw_window_hints.scaleToMonitor = true;
     spec.glfw_window_hints.decorated = true;
 
