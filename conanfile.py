c6ed195bc706b8b9207c71e9476018967d7d27c9
Daniel Paiva <danielbrug@gmail.com>
2025-10-18 17:09:58 -0300

    Review conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-
+from conan.tools.cmake import cmake_layout
 class ImguiHMI(ConanFile):
     name = "imgui-hmi"
     version = "0.1"
@@ -7,39 +7,23 @@ class ImguiHMI(ConanFile):
     url = "https://github.com/your-repo/imgui-hmi"
     description = "C++ HMI project using ImGui and OpenGL"
     settings = "os", "arch", "compiler", "build_type"
-
-    # Dependencies
-    requires = [
-        # "opengl/system",
-        "fakeit/2.4.1",
-        "catch2/3.10.0",
-        "spdlog/1.15.3",
-        "glad/0.1.36",
-        "glm/1.0.1",
-        "glfw/3.4",
-        "stb/cci.20240531",
-        "sqlite3/3.50.4",
-        "cppzmq/4.11.0",
-        "open62541pp/0.17.0",
-        "libmodbus/3.1.10"
-    ]
-
-    # Generators for CMake
     generators = "CMakeDeps", "CMakeToolchain"
 
+    def requirements(self):
+        self.requires("fakeit/2.4.1")
+        self.requires("catch2/3.10.0")
+        self.requires("spdlog/1.15.3")
+        self.requires("opengl-registry/20240721")
+        self.requires("glad/0.1.36")
+        self.requires("glm/1.0.1")
+        self.requires("glfw/3.4")
+        self.requires("stb/cci.20240531")
+        #self.requires("sqlite3/3.50.4")
+        # self.requires("cppzmq/4.11.0")
+        #self.requires("open62541pp/0.17.0")
+        #self.requires("libmodbus/3.1.10")
+    def build_requirements(self):
+        self.tool_requires("cmake/3.27.9")
+    # Generators for CMake
     def layout(self):
-        # Use cmake-layout
-        self.folders.source = "."
-        self.folders.build = "build"
-
-    def build(self):
-        # For clarity, specify paths for OpenGL in sysroot
-        opengl_include = "/opt/sysroots/beagleplay/usr/include"
-        opengl_lib = "/opt/sysroots/beagleplay/usr/lib"
-
-        self.output.info(f"Using OpenGL includes: {opengl_include}")
-        self.output.info(f"Using OpenGL libs: {opengl_lib}")
-
-    def system_requirements(self):
-        # Skip all system package installation
-        self.output.info("Skipping opengl/system installation, using sysroot")
+        cmake_layout(self)
